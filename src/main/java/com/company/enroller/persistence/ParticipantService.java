10f04586532555c2250161ef3e6d0a46f731277d
rudy <grzerudy@gmail.com>
2024-04-14 15:51:54 +0200

    pierwsze podejscie

---
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -0,0 +1,37 @@
+package com.company.enroller.persistence;
+
+import java.util.Collection;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Component;
+
+import com.company.enroller.model.Participant;
+
+@Component("participantService")
+public class ParticipantService {
+
+	DatabaseConnector connector;
+
+	public ParticipantService() {
+		connector = DatabaseConnector.getInstance();
+	}
+
+	public Collection<Participant> getAll() {
+		String hql = "FROM Participant";
+		Query query = connector.getSession().createQuery(hql);
+		return query.list();
+	}
+	public Participant findByLogin(String login){
+		return connector.getSession().get(Participant.class,login);
+	}
+	public void add(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+	public void delete(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+	}
+}
