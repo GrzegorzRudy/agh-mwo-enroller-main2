64a36a364506c2079569f1795aa63ccce8e649e8
GrzegorzRudy <111533588+GrzegorzRudy@users.noreply.github.com>
2024-07-07 16:20:16 +0200

    Update MeetingService.java
    Added  new functions

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,7 +1,6 @@
 package com.company.enroller.persistence;
 
 import java.util.Collection;
-
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
@@ -23,4 +22,25 @@ public class MeetingService {
 		return query.list();
 	}
 
+	public Meeting findById(long id) {
+		return connector.getSession().get(Meeting.class, id);
+	}
+
+	public void add(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+	}
+
+	public void delete(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(meeting);
+		transaction.commit();
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().update(meeting);
+		transaction.commit();
+	}
 }
